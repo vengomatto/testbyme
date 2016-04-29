54420c5c18e2e2140903c1e04a82c21fa36c64e6
francescon <vengomatto@gmail.com>
2016-04-29 16:22:06 +0200

    primi test

---
+++ teststopservice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASEPATH=/media/clipper/test.destination
+FILENAME=test.log
+FULLFILENAME=$BASEPATH/$FILENAME
+
+service gearman-manager stop
+
+CURRENTPATH=$(pwd)
+echo $CURRENTPATH > $FULLFILENAME
+cd $BASEPATH
+CURRENTPATH=$(pwd)
+echo $CURRENTPATH >> $FULLFILENAME
